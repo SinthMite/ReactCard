794e9ba4be4b162b1666abf39eb545bfd0eebd1a
Sinth <sinthmite@gmail.com>
2024-03-06 19:46:56 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './App.css';
 import illustration from './assets/images/illustration-article.svg';
