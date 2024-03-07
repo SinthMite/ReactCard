76a454be169d3e0190465847f6c44bda29dae878
Sinth <sinthmite@gmail.com>
2024-03-06 19:24:47 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
+import illustration from './assets/images/illustration-article.svg';
+import avatar from './assets/images/image-avatar.webp';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className="IMG">
+        <img src={illustration} alt='illustration' className="img"/>
+      </div>
+      <div className="divlearning">
+        <p className="learning">Learning</p>
+      </div>
+      <div>
+        <p>Publishing 21 Dec 2023</p>
+      </div>
+      <div>
+        <h1 className="title">HTML & CSS foundation</h1>
+      </div>
+      <div>
+        <p className="paragraph">These Languages are the backbone of every<br></br> website, defining structire, content, and<br></br> presentation</p>
+      </div>
+      <div className="author">
+        <img src={avatar}></img><p>Greg Hooper</p>
+      </div>
+    </section>
   );
 }
 
